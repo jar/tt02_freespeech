92a0bfc9d16b0621fc1d0efac110941018cdcbe6
James Ross <james.a.ross@gmail.com>
2022-11-14 00:04:56 -0500

    update

--- src/decoder.v
+++ src/decoder.v
@@ -17,22 +17,22 @@ module seg7hex (
 	always @(*) begin
 		case(hex)
 			//                   7654321
-			4'h_0: segments = 7'b0111111;
-			4'h_1: segments = 7'b0000110;
-			4'h_2: segments = 7'b1011011;
-			4'h_3: segments = 7'b1001111;
-			4'h_4: segments = 7'b1100110;
-			4'h_5: segments = 7'b1101101;
-			4'h_6: segments = 7'b1111101;
-			4'h_7: segments = 7'b0000111;
-			4'h_8: segments = 7'b1111111;
-			4'h_9: segments = 7'b1100111;
-			4'h_A: segments = 7'b1110111;
-			4'h_b: segments = 7'b1111100;
-			4'h_c: segments = 7'b1011000;
-			4'h_d: segments = 7'b1011110;
-			4'h_E: segments = 7'b1111001;
-			4'h_F: segments = 7'b1110001;
+			4'd00: segments = 7'b0111111; // 0
+			4'd01: segments = 7'b0000110; // 1
+			4'd02: segments = 7'b1011011; // 2
+			4'd03: segments = 7'b1001111; // 3
+			4'd04: segments = 7'b1100110; // 4
+			4'd05: segments = 7'b1101101; // 5
+			4'd06: segments = 7'b1111101; // 6
+			4'd07: segments = 7'b0000111; // 7
+			4'd08: segments = 7'b1111111; // 8
+			4'd09: segments = 7'b1100111; // 9
+			4'd10: segments = 7'b1110111; // A
+			4'd11: segments = 7'b1111100; // b
+			4'd12: segments = 7'b1011000; // c
+			4'd13: segments = 7'b1011110; // d
+			4'd14: segments = 7'b1111001; // E
+			4'd15: segments = 7'b1110001; // F
 		endcase
 	end
 endmodule
