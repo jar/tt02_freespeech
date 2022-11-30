a6f9eb3aee8569811533c5631e6464644f4749a8
James Ross <james.a.ross@gmail.com>
2022-11-29 21:07:24 -0500

    1k4

--- src/decoder.v
+++ src/decoder.v
@@ -20,11 +20,12 @@ module decoder (
 	wire a = code[0];
 
 	assign segments[7] = (d&!c&b&!a);
-	assign segments[6] = (d&!c&!b) | (!d&c&!a) | (!d&!c&b) | (!d&c&!b&a);
-	assign segments[5] = (d&!c&!b) | (!d&c&!a) | (!d&c&!b&a) | (!c&!b&!a);
-	assign segments[4] = (!d&b&!a) | (!c&!b&!a);
-	assign segments[3] = (!d&b&!a) | (!d&!c&b) | (!d&c&!b&a) | (!c&!b&!a);
-	assign segments[2] = (!d&b&a) | (!d&c&!a) | (!d&c&!b&a) | (!c&!b);
-	assign segments[1] = (!d&!b&!a) | (!d&b&a) | (!d&!c&b) | (!c&!b);
-	assign segments[0] = (d&!c&!b) | (!d&b&a) | (!d&b&!a) | (!d&c&!b&a) | (!c&!b&!a);
+	assign segments[6] = (!c&b&a) | (d&!c&!b) | (!d&!c&b&!a) | (c&!b&!a) | (c&!b&a) | (c&b&!a);
+	assign segments[5] = (d&!c&!b) | (c&!b&!a) | (!c&!b&!a) | (c&!b&a) | (c&b&!a);
+	assign segments[4] = (!d&!c&b&!a) | (!c&!b&!a) | (c&b&!a);
+	assign segments[3] = (!c&b&a) | (!d&!c&b&!a) | (!c&!b&!a) | (c&!b&a) | (c&b&!a);
+	assign segments[2] = (!c&!b) | (c&!b&!a) | (b&a) | (c&!b&a) | (c&b&!a);
+	assign segments[1] = (!c&!b) | (!d&!c&b&!a) | (c&!b&!a) | (b&a);
+	assign segments[0] = (d&!c&!b) | (!d&!c&b&!a) | (b&a) | (!c&!b&!a) | (c&!b&a) | (c&b&!a);
+
 endmodule
